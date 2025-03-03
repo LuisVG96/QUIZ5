1a9f678eaf7b61161ab2e342d955bb137702a87c
Luis Vásquez <luisv5382@gmail.com>
2025-03-03 13:23:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const div1 = document.getElementById('div1');
+    const div2 = document.getElementById('div2');
+    const div3 = document.getElementById('div3');
+  
+    
+    div1.textContent = 'Dont worry about a thing';
+    div2.textContent = 'Cause every little thing';
+    div3.textContent = 'Gonna be alright.';
+  });
